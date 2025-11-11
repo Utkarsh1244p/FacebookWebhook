b8c8468792a1bf49e53178c2695186d082d38e4c
Utkarsh Gayguwal <utkarshgayguwal.tg@gmail.com>
2025-11-11 11:37:07 +0000

    feat: horizon package added.

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Jobs\PaymentJob;
 use App\Jobs\SendWelcomeEmail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     SendWelcomeEmail::dispatch();
+    PaymentJob::dispatch();
     return view('welcome');
 });
