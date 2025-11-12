ffa55a0d67d4b4f65e07332c749685c216f5404a
Utkarsh Gayguwal <utkarshgayguwal.tg@gmail.com>
2025-11-12 12:39:13 +0000

    chore: added api.php

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,25 @@
 
 use App\Jobs\PaymentJob;
 use App\Jobs\SendWelcomeEmail;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Bus;
 
 Route::get('/', function () {
-    SendWelcomeEmail::dispatchSync();
-    PaymentJob::dispatch()->onQueue('payment');
+    // Bus::batch([
+    //     new SendWelcomeEmail(),
+    //     new PaymentJob(),
+    // ])
+    // ->allowFailures()
+    // ->catch(function ($batch, $e) {
+    //     info('Batch failed: ' . $e->getMessage());
+    // })
+    // ->finally(function ($batch) {
+    //     info('Batch completed with ' . $batch->totalJobs . ' jobs');
+    // })
+    // ->dispatch();
+
     return view('welcome');
 });
+
+
